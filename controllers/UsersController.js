34ea04e1994ee8530415f77a9e2910a6627b47e7
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-03-21 20:22:32 -0300

    Criando validações

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,9 +1,43 @@
 class UsersController {
-    
+
     async create(req, res) {
+        let { email, name, password } = req.body;
+        let emailError;
+        let nameError;
+        let passwordError;
 
+        if (email == undefined || email == "") {
+            emailError = "E-mail invalido ou indefinido!"
+            res.json({ err: emailError });
+            return;
+        }
+
+        if (name == undefined || name == "") {
+            nameError = "Nome invalido ou indefinido!"
+            res.json({ err: nameError });
+            return;
+        }
+
+        if (name.length < 4) {
+            nameError = "Nome não pode conter menos que 4 caracteres!"
+            res.json({ err: nameError });
+            return;
+        }
+
+        if (password == undefined || password == "") {
+            passwordError = "Senha invalida ou indefinida!"
+            res.json({ err: passwordError });
+            return;
+        }
+
+        if(password.length < 5){
+            passwordError = "Senha não pode conter menos de 5 caracteres!"
+            res.json({ err: passwordError });
+            return;
+        }
+
+        res.send("Dados recebidos com sucesso!");
     }
-
 }
 
 module.exports = new UsersController();
