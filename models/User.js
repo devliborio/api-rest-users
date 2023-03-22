54d9b6991763d30e2ab14ba0331584cad8b66c25
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-03-22 12:32:20 -0300

    Usando bcrypt para transformar a senha em hash

--- models/User.js
+++ models/User.js
@@ -4,14 +4,13 @@ const bcrypt = require('bcrypt');
 class User {
 
     async new(email, password, name) {
-
         try {
-            await knex.insert({ email, password, name, role: 0 }).table('users')
+            let hash = await bcrypt.hash(password, 10); // Primeiro parâmetro a senha e quantas vezes vamos "hashear" ela.
+            await knex.insert({ email, password: hash, name, role: 0 }).table('users')
         } catch (err) {
             console.log(err)
         }
     }
-
 }
 
 module.exports = new User()
